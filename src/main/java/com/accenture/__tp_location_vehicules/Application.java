87b13d63be0ab21867bb36764be95bf96d4ba8d1
Thébault <elian.thebault@accenture.com>
2025-02-20 10:07:16 +0100

    another commit

---
+++ src/main/java/com/accenture/__tp_location_vehicules/Application.java
@@ -0,0 +1,13 @@
+package com.accenture.__tp_location_vehicules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/__tp_location_vehicules/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.__tp_location_vehicules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
